ac06fe9997eb032581c009a4a69ab8819561c35a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-07 22:03:14 +0530

    hide rapid api key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_YT_API_KEY,
   },
 };
 
